29e47d7c2ae8a3abbc5c00a3ed2e260e326d6842
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2024-11-13 15:20:55 +0700

    update sql

--- library_management.sql
+++ library_management.sql
@@ -121,6 +121,11 @@ CREATE TABLE PhieuMuonChiTiet (
     FOREIGN KEY (ISBN) REFERENCES Sach(ISBN)
 );
 
+
+
+
+
+
 -- Trigger
 -- Kiểm tra tính hợp lệ user
 DROP TRIGGER IF EXISTS trg_check_user_fields
@@ -177,6 +182,61 @@ BEGIN
 END $$
 DELIMITER ;
 
+-- Kiểm tra tính hợp lệ chủ đề
+DROP TRIGGER IF EXISTS trg_check_chude_fields
+DROP TRIGGER IF EXISTS trg_check_chude_fields_update
+DELIMITER $$
+CREATE TRIGGER trg_check_chude_fields
+BEFORE INSERT ON ChuDe
+FOR EACH ROW
+BEGIN
+    IF NEW.TenChuDe IS NULL OR NEW.TenChuDe = '' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Tên chủ đề không được để trống';
+    END IF;
+END $$
+
+CREATE TRIGGER trg_check_chude_fields_update
+BEFORE UPDATE ON ChuDe
+FOR EACH ROW
+BEGIN
+    IF NEW.TenChuDe IS NULL OR NEW.TenChuDe = '' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Tên chủ đề không được để trống';
+    END IF;
+END $$
+DELIMITER ;
+
+-- Kiểm tra tính hợp lệ Nhà xuất bản
+DROP TRIGGER IF EXISTS trg_check_publisher_fields
+DROP TRIGGER IF EXISTS trg_check_publisher_fields_update
+DELIMITER $$
+CREATE TRIGGER trg_check_publisher_fields
+BEFORE INSERT ON NXB
+FOR EACH ROW
+BEGIN
+    IF NEW.TenNXB IS NULL OR NEW.TenNXB = '' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Tên nhà xuất bản không được để trống';
+    END IF;
+END $$
+
+CREATE TRIGGER trg_check_publisher_fields_update
+BEFORE UPDATE ON NXB
+FOR EACH ROW
+BEGIN
+    IF NEW.TenNXB IS NULL OR NEW.TenNXB = '' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Tên nhà xuất bản không được để trống';
+    END IF;
+END $$
+DELIMITER ;
+
+
+
+
+
+
 -- Function
 -- kiểm tra user tồn tại?
 DROP FUNCTION IF EXISTS check_user_exists
@@ -197,7 +257,43 @@ BEGIN
 END$$
 DELIMITER ;
 
+-- Kiểm tra thể loại đã có chưa?
+DROP FUNCTION IF EXISTS check_genres_exists
+DELIMITER $$
+CREATE FUNCTION check_genres_exists(value VARCHAR(255))
+RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+    DECLARE genresExists BOOLEAN DEFAULT FALSE;
+    
+        SET genresExists = EXISTS (SELECT 1 FROM ChuDe WHERE TenChuDe = value);
+    
+    RETURN genresExists;
+END$$
+DELIMITER ;
+
+-- Kiểm tra nhà xuất bản có chưa ?
+DROP FUNCTION IF EXISTS check_publisher_exists
+DELIMITER $$
+CREATE FUNCTION check_publisher_exists(value VARCHAR(255))
+RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+    DECLARE publisherExists BOOLEAN DEFAULT FALSE;
+
+        SET publisherExists = EXISTS (SELECT 1 FROM NXB WHERE TenNXB = value);
+    
+    RETURN publisherExists;
+END$$
+DELIMITER ;
+
+
+
+
+
+
 -- Procedure
+-- Tạo tài khoản
 DROP PROCEDURE IF EXISTS create_account;
 DELIMITER //
 
@@ -246,6 +342,100 @@ account_label: BEGIN -- Đặt nhãn cho BEGIN...END
     -- Trả về kết quả thành công
     SET p_status = 0;
     SET p_message = 'Đăng ký thành công!';
+END //
+
+DELIMITER ;
+
+-- Tạo chủ đề
+DROP PROCEDURE IF EXISTS create_chude;
+DELIMITER //
+
+CREATE PROCEDURE create_chude (
+    IN p_tenchude VARCHAR(255),
+    OUT p_status INT,
+    OUT p_message VARCHAR(255)
+)
+chude_label: BEGIN
+    DECLARE max_id VARCHAR(10);
+    DECLARE new_id VARCHAR(10);
+
+    -- Kiểm tra chủ đề có tồn tại hay không
+    IF EXISTS (SELECT 1 FROM ChuDe WHERE TenChuDe = p_tenchude) THEN
+        SET p_status = 1;
+        SET p_message = 'Chủ đề đã tồn tại!';
+        LEAVE chude_label;
+    END IF;
+
+    -- Xác định mã chủ đề lớn nhất hiện tại với tiền tố "CD"
+    SET max_id = (
+        SELECT MAX(ChuDeID)
+        FROM ChuDe
+        WHERE ChuDeID LIKE 'CD%'
+    );
+
+    -- Tạo mã mới nếu max_id không null, ngược lại dùng mã mặc định CD01
+    IF max_id IS NOT NULL THEN
+        SET new_id = CONCAT('CD', LPAD(SUBSTRING(max_id, 3) + 1, 2, '0'));
+    ELSE
+        SET new_id = 'CD01';
+    END IF;
+
+    -- Thêm chủ đề mới vào database
+    INSERT INTO ChuDe (ChuDeID, TenChuDe)
+    VALUES (new_id, p_tenchude);
+
+    -- Trả về kết quả thành công
+    SET p_status = 0;
+    SET p_message = 'Tạo chủ đề thành công!';
+END //
+
+DELIMITER ;
+
+-- Tạo nhà xuất bản
+DROP PROCEDURE IF EXISTS create_nxb;
+DELIMITER //
+
+CREATE PROCEDURE create_nxb (
+    IN p_tennxb VARCHAR(255),
+    IN p_diachi VARCHAR(255),
+    IN p_email VARCHAR(255),
+    IN p_infonguoidaidien VARCHAR(255),
+    
+    OUT p_status INT,
+    OUT p_message VARCHAR(255)
+)
+nxb_label: BEGIN
+    DECLARE max_id VARCHAR(10);
+    DECLARE new_id VARCHAR(10);
+
+    -- Kiểm tra nhà xuất bản có tồn tại hay không
+    IF EXISTS (SELECT 1 FROM NXB WHERE TenNXB = p_tennxb) THEN
+        SET p_status = 1;
+        SET p_message = 'Nhà xuất bản đã tồn tại!';
+        LEAVE nxb_label;
+    END IF;
+
+    -- Xác định mã nhà xuất bản lớn nhất hiện tại với tiền tố "NXB"
+    SET max_id = (
+        SELECT MAX(NXB_ID)
+        FROM NXB
+        WHERE NXB_ID LIKE 'NXB%'
+    );
+
+    -- Tạo mã mới nếu max_id không null, ngược lại dùng mã mặc định NXB01
+    IF max_id IS NOT NULL THEN
+        SET new_id = CONCAT('NXB', LPAD(SUBSTRING(max_id, 3) + 1, 2, '0'));
+    ELSE
+        SET new_id = 'NXB01';
+    END IF;
+
+    -- Thêm nhà xuất bản mới vào database
+    INSERT INTO NXB (NXB_ID, TenNXB, DiaChi, Email, Info_ngDaiDien)
+    VALUES (new_id, p_tennxb, p_diachi, p_email, p_infonguoidaidien);
+
+    -- Trả về kết quả thành công
+    SET p_status = 0;
+    SET p_message = 'Tạo nhà xuất bản thành công!';
 END //
 
 DELIMITER ;
